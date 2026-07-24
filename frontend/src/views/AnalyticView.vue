c9042f1d789aca0bf7b9cc6333027415b37a7c53
kodibeol-netizen <kodibeol@gmail.com>
2026-07-24 10:06:25 +0100

    Delete frontend directory

--- frontend/src/views/AnalyticView.vue
+++
@@ -1,8 +0,0 @@
-<!-- 1. Importation du Template HTML -->
-<template src="./AnalyticView.html"></template>
-
-<!-- 2. Importation du Script JavaScript -->
-<script src="./AnalyticView.js"></script>
-
-<!-- 3. Importation du Style CSS Scoped (Propre à ce composant) -->
-<style scoped src="./AnalyticView.css"></style>
